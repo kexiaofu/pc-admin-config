897fa15c71e51bb127cccfdb770a0c924b45cd58
liangjf <liangjf@yingmi.cn>
2021-09-09 14:48:47 +0800

    change build config and add components dir

--- .umirc.ts
+++ .umirc.ts
@@ -1,4 +1,8 @@
 import { defineConfig } from 'umi';
+const { REACT_APP_ENV, NODE_ENV } = process.env;
+
+const IS_DEV_ENV = NODE_ENV === 'development';
+
 
 export default defineConfig({
   nodeModulesTransform: {
@@ -9,4 +13,53 @@ export default defineConfig({
     { path: '/', component: '@/pages/index' },
   ],
   fastRefresh: {},
+  mfsu: {},
+  ...(IS_DEV_ENV ? {
+    nodeModulesTransform: {
+      type: 'none',
+      exclude: [],
+    },
+    webpack5: {
+      lazyCompilation: {
+        imports: true,
+      }
+    }
+  } : {
+    chunks: ['vendors', 'umi', 'ant'],
+    chainWebpack: function (config, { webpack }) {
+      config.merge({
+        optimization: {
+          splitChunks: {
+            chunks: 'all',
+            minSize: 30000,
+            minChunks: 2,
+            automaticNameDelimiter: '.',
+            cacheGroups: {
+              umi: {
+                name: 'umi',
+                test({ resource }) {
+                  return /(umi).*/.test(resource);
+                },
+                priority: 1,
+              },
+              vendor: {
+                name: 'vendors',
+                test({ resource }) {
+                  return /[\\/]node_modules[\\/]/.test(resource);
+                },
+                priority: 10,
+              },
+              ant: {
+                name: 'ant',
+                test({ resource }) {
+                  return /(ant)/.test(resource);
+                },
+                priority: 100,
+              },
+            },
+          },
+        },
+      });
+    },
+  })
 });
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
 import styles from './index.less';
+import {useEffect} from 'react';
 
 export default function IndexPage() {
+  useEffect(() => {
+    console.log('---> on --> ')
+  }, [])
   return (
     <div>
-      <h1 className={styles.title}>Page index123</h1>
+      <h1 className={styles.title}>Page index 876 hello ?</h1>
     </div>
   );
 }
